ec46443c753ad1238dddf64c99cbea1bc591440a
Dushyant Chauhan <dushyantr737@gmail.com>
2024-10-28 21:11:02 +0530

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,13 @@
+
+body:Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color(0xffffecd2),Color(0xffcb69f)
+            ],
+            begin: FractionalOffset(1.0,0.5),
+            end: FractionalOffset(0.0, 0.5),
+              stops: [0.0,0.0,1.0]
+          )
+        )
+      )
